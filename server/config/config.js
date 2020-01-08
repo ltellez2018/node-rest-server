e41ff1ccdca3818e60db8a926a38d3c91342fe52
Luis Alberto Tellez Olguin <luis.tellez@stkinfonavit.com>
2020-01-08 12:19:41 -0600

    variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 }else {
-	urlDB = 'mongodb+srv://Leomich:2L87Al0gmHi2y5a8@cluster0-yvlci.mongodb.net/cafe?retryWrites=true&w=majority';
+	urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
