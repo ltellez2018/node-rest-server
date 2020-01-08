3531926799ac2e9b150378628a63850f176449c6
Luis Alberto Tellez Olguin <luis.tellez@stkinfonavit.com>
2020-01-08 11:57:24 -0600

    cambios en el config

--- server/config/config.js
+++ server/config/config.js
@@ -5,13 +5,15 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
+
 // * B A S E   D E  D A T O S
 let urlDB;
 //urlDB = 'mongodb+srv://Leomich:2L87Al0gmHi2y5a8@cluster0-yvlci.mongodb.net/cafe?retryWrites=true&w=majority';
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 }else {
-	urlDB = 'mongodb + srv://Leomich:2L87Al0gmHi2y5a8@cluster0-yvlci.mongodb.net/cafe';
+	urlDB = 'mongodb+srv://Leomich:2L87Al0gmHi2y5a8@cluster0-yvlci.mongodb.net/cafe?retryWrites=true&w=majority';
 }
 
 process.env.URLDB = urlDB;
+
